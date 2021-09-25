2ec10bcf4395e08e7a496ea857a4f1f423e613da
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2021-09-25 13:25:53 +0100

    Pull event code into its own namespace

---
+++ src/chess/events.clj
@@ -0,0 +1,40 @@
+(ns chess.events
+  (:require
+   [clojure.string :as str]
+   [java-time])
+  (:import java.io.File))
+
+(declare
+ file-names events as-event event-name event-date)
+
+(defn find-events [events-directory]
+  (let [directories
+        (file-names
+         (.listFiles
+          (new File events-directory)))]
+    (events directories)))
+
+(defn events [directories]
+  (map as-event directories))
+
+(defn as-event [directory]
+  (let [parts
+        (str/split directory #"-")]
+
+    {:name (event-name (drop-last 3 parts))
+     :date (event-date (take-last 3 parts))}))
+
+(defn file-names [files]
+  (map (fn [file] (.getName file)) files))
+
+(defn event-name [name-parts]
+  (str/join
+   " "
+   (map str/capitalize name-parts)))
+
+(defn event-date [date-parts]
+  (apply
+   java-time/local-date
+   (map
+    (fn [string] (Integer/parseInt string))
+    date-parts)))
--- src/chess/routes.clj
+++ src/chess/routes.clj
@@ -3,12 +3,10 @@
    [hiccup.page :as page]
    [clojure.string :as str]
    [java-time]
-   )
+   [chess.events :refer :all])
   (:import java.io.File))
 
-(declare
- find-events file-names events as-event event-name event-date
- links link-text)
+(declare links link-text)
 
 (defn homepage []
   (page/html5
@@ -25,37 +23,6 @@
     (page/html5
      [:body (links events)])))
 
-(defn find-events [events-directory]
-  (let [directories
-        (file-names
-         (.listFiles
-          (new File events-directory)))]
-    (events directories)))
-
-(defn events [directories]
-  (map as-event directories))
-
-(defn as-event [directory]
-  (let [parts
-        (str/split directory #"-")]
-
-    {:name (event-name (drop-last 3 parts))
-     :date (event-date (take-last 3 parts))}))
-
-(defn file-names [files]
-  (map (fn [file] (.getName file)) files))
-
-(defn event-name [name-parts]
-  (str/join
-   " "
-   (map str/capitalize name-parts)))
-
-(defn event-date [date-parts]
-  (apply
-   java-time/local-date
-   (map
-    (fn [string] (Integer/parseInt string))
-    date-parts)))
 
 (defn links [events]
   (map
