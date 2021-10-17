4231edc1d30e94538461001f0301c00173c31a6c
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2021-10-17 19:42:11 +0100

    Use eftest again, but make tests run in serial

--- project.clj
+++ project.clj
@@ -13,4 +13,6 @@
   :main ^:skip-aot chess.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}
+
+  :eftest {:multithread? false})
