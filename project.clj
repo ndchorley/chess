9881831ebc6f40f5921dee5fe0f6ff69833cbf49
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-06-01 10:49:52 +0100

    Upgrade hiccup to 2.0.0-RC3

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
                  [ring/ring-jetty-adapter "1.12.1"]
                  [ring/ring-mock "0.4.0"]
                  [org.jsoup/jsoup "1.17.2"]
-                 [hiccup "1.0.5"]
+                 [hiccup "2.0.0-RC3"]
                  [compojure "1.7.0"]
                  [clojure.java-time "1.2.0"]
                  [com.github.bhlangonijr/chesslib "1.3.3"]
