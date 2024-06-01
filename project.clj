e7ca7a57c76cf99e2e696f334507183e72636441
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2024-06-01 10:45:26 +0100

    Upgrade clojure to 1.11.3

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject chess "0.1.0-SNAPSHOT"
   :repositories [["jitpack.io" "https://jitpack.io"]]
   :dependencies [
-                 [org.clojure/clojure "1.11.1"]
+                 [org.clojure/clojure "1.11.3"]
                  [ring/ring-core "1.10.0"]
                  [ring/ring-jetty-adapter "1.10.0"]
                  [ring/ring-mock "0.4.0"]
